519756fd1dd464f543fab067f6eacd23ef69cf02
LuisPaucarcaja <luispaucarcajagarcia@gmail.com>
2026-01-19 15:38:16 -0500

    Revert "feat(reserva): agrega ReservaService (work in progress)"
    This reverts commit b4878e366294618fed84f8480f7dd06c4804a42f.

--- src/main/java/com/indra/reservations_backend/dto/ReservaResponseDto.java
+++ src/main/java/com/indra/reservations_backend/dto/ReservaResponseDto.java
@@ -3,11 +3,9 @@ package com.indra.reservations_backend.dto;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
